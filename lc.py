9e064e12321f7e4ad6e590ac63b9346967045ef6
philip-mv <philipmv10@gmail.com>
2025-03-06 16:28:06 +0530

    jmmnmbmn

--- lc.py
+++ lc.py
@@ -1,6 +1,6 @@
 #Without List Comprehension
 
-fruits = ["apple", "banana", "cherry", "kiwi", "mango"] 
+fruits = ["apple", "grape", "cherry", "kiwi", "bomboclat"] 
 newlist = [] 
 for x in fruits: 
  if "a" in x: 
@@ -9,6 +9,6 @@ print(newlist)
 
 #With List Comprehension
 
-fruits = ["apple", "banana", "cherry", "kiwi", "mango"] 
+fruits = ["apple", "grape", "cherry", "kiwi", "bomboclat"] 
 newlist = [x for x in fruits if "a" in x] 
 print(newlist)
